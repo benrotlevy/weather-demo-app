198b0d7484b443cb3e76bcea815c9aa4524580bc
benrotlevy <benjamin@rotlevy.com>
2023-11-01 19:20:13 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+const app = express();
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("/api/weather/:city", cors(), async (req, res) => {
+    try {
+        const response = await axios.get(
+            "https://api.weatherapi.com/v1/current.json",
+            {
+                params: {
+                    key: process.env.API_KEY,
+                    q: req.params.city,
+                },
+            }
+        );
+        const obj = {
+            temp_c: response.data.current.temp_c,
+            temp_f: response.data.current.temp_f,
+            last_updated: response.data.current.last_updated,
+            city: response.data.location.name,
+        };
+        res.json(obj);
+    } catch (error) {
+        if (error.response.status === 400) {
+            res.status(400).send(error.response.data.error.message);
+        } else {
+            res.status(500).send("failed to fetch data from server");
+        }
+    }
+});
+
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, () => {
+    console.log(`listening to port: ${PORT}`);
+});
---
+++ client/src/App.js
@@ -0,0 +1,68 @@
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+import icon from "./icon.jpeg";
+
+function App() {
+    const [data, setData] = useState("");
+    const [inputValue, setInputValue] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const search = async () => {
+        let url = "/api/weather/";
+        if (process.env.NODE_ENV === "development") {
+            console.log(process.env.NODE_ENV);
+            url = "http://localhost:7000/api/weather/";
+        }
+        try {
+            setLoading(true);
+            const { data } = await axios.get(`${url}${inputValue}`);
+            console.log(data);
+            setLoading(false);
+            setData(data);
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+            setError(error.message);
+        }
+    };
+
+    if (loading) {
+        return <h1>Loading...</h1>;
+    }
+
+    if (error) {
+        return <h1>{error}</h1>;
+    }
+
+    return (
+        <div className="App">
+            <header>
+                <img src={icon} width="100" height="100" />
+            </header>
+            <input
+                placeholder="enter city name"
+                value={inputValue}
+                onChange={({ target }) => setInputValue(target.value)}
+            />
+            <button onClick={search}>search</button>
+            {data.city && (
+                <div>
+                    <h2>Current Weather at {data.city}</h2>
+                    <p>
+                        Celzius Temperature: <b>{data.temp_c}</b>
+                    </p>
+                    <p>
+                        Fernhait Temperature: <b>{data.temp_f}</b>
+                    </p>
+                    <p>
+                        last updated at <b>{data.last_updated}</b>
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default App;
